f285f4e3a27d26f39f31297ee964257c31431d82
yu1ia <vasilenyuky@gmail.com>
2022-12-05 13:24:10 +0200

    Updates

---
+++ precache-manifest.f3a2c1d0a5ed2321df251aea82000887.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee720ce095cfa037202464cc8443933",
+    "url": "/blobio2/index.html"
+  },
+  {
+    "revision": "177e26a460e332501cf5",
+    "url": "/blobio2/static/css/main.032f194d.chunk.css"
+  },
+  {
+    "revision": "e7e8a8ae6adfaad1d682",
+    "url": "/blobio2/static/js/2.e0e57e32.chunk.js"
+  },
+  {
+    "revision": "177e26a460e332501cf5",
+    "url": "/blobio2/static/js/main.4c61c07d.chunk.js"
+  },
+  {
+    "revision": "94c4acf344cff23e06b4",
+    "url": "/blobio2/static/js/runtime~main.063d0139.js"
+  }
+]);
